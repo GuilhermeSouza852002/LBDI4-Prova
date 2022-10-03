39184de6a3cf4fe0fcc6f4b336ff8e0fd927b517
Guilherme O. Souza <ShadowDragonRub@gmail.com>
2022-10-03 18:29:42 -0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	orm/mapping_single.py

--- orm/mapping_single.py
+++ orm/mapping_single.py
@@ -14,7 +14,7 @@ Base = declarative_base()   #construção de uma classe base para definir classe
 class Jogador(Base):
     __tablename__ = "Jogador"
     id_jogador = Column(Integer, primary_key=True)
-    nome = Column(String(150), nullable=False)
+    nome = Column(String(60), nullable=False)
 
 class Personagem(Base):
     __tablename__ = "Personagem"
@@ -39,11 +39,11 @@ class Inventario(Base):
 class Item(Base):
     __tablename__ = "Item"
     id_item = Column(Integer, primary_key=True)
-    efeito = Column(String(150), nullable=False)
+    efeito = Column(String(60), nullable=False)
     raridade = Column(Integer, nullable=False)
     requer_forca = Column(Integer, nullable=False)
     requer_destreza = Column(Integer, nullable=False)
-    descricao = Column(String(200), nullable=False)
+    descricao = Column(String(60), nullable=False)
 
 class Arma(Base):
     __tablename__ = "Arma"
@@ -67,17 +67,17 @@ class Mapa(Base):
 class Cenario(Base):
     __tablename__ = "Cenario"
     id_cenario = Column(Integer, primary_key=True)
-    descricao = Column(String(200), nullable=False)
+    descricao = Column(String(60), nullable=False)
 
 class Clima(Base):
     __tablename__ = "Clima"
     id_clima = Column(Integer, primary_key=True)
-    descricao = Column(String(200), nullable=False)
+    descricao = Column(String(60), nullable=False)
 
 class Inimigo(Base):
     __tablename__ = "Inimigo"
     id_inimigo = Column(Integer, primary_key=True)
-    nome_inimigo = Column(String(150), nullable=False)
+    nome_inimigo = Column(String(60), nullable=False)
     hp = Column(Integer, nullable=False)
     experiencia = Column(Integer, nullable=False)
 
@@ -100,52 +100,52 @@ def main():
         session.add(jogador)
 
     with Session.begin() as session:
-        personagem = Personagem(hp=100, level=1, forca=10, destreza=11, classe='paladino')
+        personagem = Personagem(hp=100, level=1, forca=10, destreza=11, classe="paladino")
         id_personagem = personagem.id_personagem
         session.add(personagem)
 
     with Session.begin() as session:
-        habilidade = Habilidade(nome='Raio de luz solar', tipo='Milagre')
+        habilidade = Habilidade(nome="Raio de luz solar", tipo="Milagre")
         id_habilidade = habilidade.id_habilidade
         session.add(habilidade)
 
     with Session.begin() as session:
-        inventario = Inventario(tipo='Utilidade')
+        inventario = Inventario(tipo="Utilidade")
         id_inventario = inventario.id_inventario
         session.add(inventario)
 
     with Session.begin() as session:
-        item = Item(efeito='Fogo', raridade=3, requer_forca=10, requer_destrza=11, descricao='espada de fogo')
+        item = Item(efeito="Fogo", raridade=3, requer_forca=10, requer_destrza=11, descricao="espada de fogo")
         id_item = item.id_item
         session.add(item)
 
     with Session.begin() as session:
-        arma = Arma(nome_arma='Espada de Gwyn', alcance=5, dano=20)
+        arma = Arma(nome_arma="Espada de Gwyn", alcance=5, dano=20)
         id_arma = arma.id_arma
         session.add(arma)
 
     with Session.begin() as session:
-        armadura = Armadura(nome_armadura='Traje real', tipo='Pesado', defesa=15)
+        armadura = Armadura(nome_armadura="Traje real", tipo="Pesado", defesa=15)
         id_armadura = armadura.id_armadura
         session.add(armadura)
 
     with Session.begin() as session:
-        mapa = Mapa(nome_mapa='Anor Londo')
+        mapa = Mapa(nome_mapa="Anor Londo")
         id_mapa = mapa.id_mapa
         session.add(mapa)
 
     with Session.begin() as session:
-        cenario = Cenario(descricao='Um lugar belo e iluminado')
+        cenario = Cenario(descricao="Um lugar belo e iluminado")
         id_cenario = cenario.id_cenario
         session.add(cenario)
 
     with Session.begin() as session:
-        clima = Clima(descricao='Ensolarado')
+        clima = Clima(descricao="Ensolarado")
         id_clima = clima.id_clima
         session.add(clima)
 
     with Session.begin() as session:
-        inimigo = Inimigo(nome_inimigo='Kalameet', hp=250, experiencia=3500)
+        inimigo = Inimigo(nome_inimigo="Kalameet", hp=250, experiencia=3500)
         id_inimigo = inimigo.id_inimigo
         session.add(inimigo)
 
